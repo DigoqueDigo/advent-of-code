c47b2a3f4654294bcd5207e069a3d7f0510ba8a4
DigoqueDigo <diogoacmarques2003@gmail.com>
2025-12-02 22:29:58 +0000

    Improve day02

--- day02.rb
+++ day02.rb
@@ -4,19 +4,19 @@ while ranges = STDIN.gets
     ranges.split(',').each do |range|
         first, last = range.split('-').map(&:to_i)
 
-        while first <= last
-            first_s = first.to_s
-            first_s_len = first_s.length
-            chunk_sizes = (1..(first_s_len / 2)).select { |n| first_s_len % n == 0 }
+        (first..last).each() do |current|
+            current_s = current.to_s
+            current_s_len = current_s.length
+            chunk_sizes = (1..(current_s_len / 2))
+                .select { |n| current_s_len % n == 0 }
 
             chunk_sizes.each do |chunk_size|
-                if first_s.scan(/.{1,#{chunk_size}}/).uniq.length == 1
-                    invalid_ids_sum += first
+                if current_s.scan(/.{1,#{chunk_size}}/).uniq.length == 1
+                    invalid_ids_sum += current
+                    puts current
                     break
                 end
             end
-
-            first += 1
         end
     end
 end
