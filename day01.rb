66685f80a8ff96aee2c7309074bc37e3391f62d6
DigoqueDigo <diogoacmarques2003@gmail.com>
2025-12-02 00:24:34 +0000

    day 1

---
+++ day01.rb
@@ -0,0 +1,25 @@
+dial = 50
+zeros = 0
+
+while line = STDIN.gets
+    direction = line[0]
+    rotation = line[1..-1].to_i
+    zeros += rotation / 100
+
+    if direction == 'L'
+        rotation = -rotation
+    end
+
+    old_dial = dial
+    dial = (dial + rotation) % 100
+
+    if dial == 0
+        zeros += 1
+    elsif direction == 'R' and old_dial > dial and old_dial != 0
+        zeros += 1
+    elsif direction == 'L' and old_dial < dial and old_dial != 0
+        zeros += 1
+    end
+end
+
+puts "Password: #{zeros}" #6684
