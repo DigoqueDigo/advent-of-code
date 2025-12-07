5d006e561b2fc8bdafb56bcb9ee524e0252ad1d8
DigoqueDigo <diogoacmarques2003@gmail.com>
2025-12-07 00:29:36 +0000

    Refactor invalid ID detection logic for improved clarity and performance

--- challenges/2025/02/solution.rb
+++ challenges/2025/02/solution.rb
@@ -4,47 +4,61 @@ module Year2025
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
-    def find_invalid_ids(first, last, repetitions = nil)
-      invalid_ids = Array.new
-      (first..last).each do |current|
-        current_s = current.to_s
-        current_s_len = current_s.length
-        chunk_sizes = (1..(current_s_len / 2))
-            .select { |n| current_s_len % n == 0 }
+    def sum_invalid_ids(range, repetitions = nil)
+      total = 0
 
-        chunk_sizes.each do |chunk_size|
-          chunks = current_s.scan(/.{1,#{chunk_size}}/)
-          if (chunks.length == repetitions or !repetitions) and chunks.uniq.length == 1
-            invalid_ids.push(current)
+      range.each do |current|
+        s = current.to_s
+        len = s.length
+        max_chunk = len / 2
+
+        (1..max_chunk).each do |chunk|
+          next unless (len % chunk).zero?
+
+          reps = len / chunk
+          next if repetitions and repetitions != reps
+
+          i = chunk
+          piece = s[0, chunk]
+          valid = true
+
+          while i < len
+            if s[i, chunk] != piece
+              valid = false
+              break
+            end
+            i += chunk
+          end
+
+          if valid
+            total += current
             break
           end
         end
       end
-      invalid_ids
+
+      total
     end
 
     def part_1
-      invalid_ids_sum = 0
-      data.each do |range|
-        first, last = range.split('-').map(&:to_i)
-        invalid_ids_sum += find_invalid_ids(first, last, 2).sum
-      end
-      invalid_ids_sum
+      data.sum { |range| 
+        sum_invalid_ids(range, 2)
+      }
     end
 
     def part_2
-      invalid_ids_sum = 0
-      data.each do |range|
-        first, last = range.split('-').map(&:to_i)
-        invalid_ids_sum += find_invalid_ids(first, last).sum
-      end
-      invalid_ids_sum
+      data.sum { |range| 
+        sum_invalid_ids(range)
+      }
     end
 
     private
       # Processes each line of the input file and stores the result in the dataset
       def process_input(line)
-        line.split(',')
+        line.split(',').map do |range|
+          first, last = range.split('-').map(&:to_i)
+          first..last
+        end
       end
 
       # Processes the dataset as a whole
